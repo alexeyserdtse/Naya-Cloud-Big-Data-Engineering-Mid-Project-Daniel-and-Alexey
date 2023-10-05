93b4ba39609d4991904f6539cd7cb5335dab6e51
Alexey <alexeyserdtse90@gmail.com>
2023-10-05 20:50:26 +0300

    Alexey - Get Data From  Api

---
+++ main.py
@@ -0,0 +1,109 @@
+import requests
+import json
+import pandas as pd
+import sys
+import time
+from datetime import datetime
+
+
+def load_data_subset(url):
+    try:
+        # Open new connection:
+        response = requests.get(url)
+        if response.status_code == 200:
+            try:
+                data = response.json()
+                response.close()
+            except Exception as link_error:
+                print(link_error)
+        else:
+            err = (f"\n{'*'*65}\nError in response: \n\nStatus from API: {response.status_code}"
+                   f"\nError: \n{response.text}\nLink: {url}\n\n{'*'*65}")
+            print(err)
+
+        # re-write to json file:
+        with open('data.json', 'w') as f:
+            f.truncate(0)
+            json.dump(data, f)
+
+        # json to pandas df:
+        df = pd.read_json('data.json', orient='records')
+    except Exception as e:
+        print(f'\nAn error occurred: \n{e}\n\nLink: {url}\n')
+        sys.exit()
+
+    return df
+
+
+def load_full_data(url):
+    crime_df = pd.DataFrame()
+    fetch_full_data, offset_mul = True, 0
+    start_time = time.time()
+
+    if fetch_full_data == True:
+        # init starting link:
+        url = url
+
+        while True:
+            inc_df = load_data_subset(url)
+
+            # run until api retrieve data:
+            if len(inc_df) > 0:
+                crime_df = pd.concat([crime_df, inc_df])
+
+                # increase offset from previous iteration
+                offset_mul += 1
+
+                # create next-link (increase offset for next iteration)
+                url = url[:103] + f'&$offset={80000 * offset_mul}'
+                print(f'Loaded: {len(crime_df):,}')
+            else:
+                break
+
+        print('Done!')
+        end_time = time.time()
+
+    comleted_load_msg = ("\n"
+                         f"{'*'*108}\nLoad succesed! Start time: {datetime.now()}, elapsed time (sec): {(end_time - start_time):.2f}, "
+                         f"Total Rows Loaded: {len(crime_df):,}\n{'*'*108}"
+                         )
+
+    print(comleted_load_msg)
+    return crime_df
+
+
+def clean_data(df):
+    # include eda + cleaning of the data
+    pass
+
+
+def transform_data(df):
+    pass
+
+
+def render_graph_html(df):
+    pass
+
+
+def main(url):
+    raw_data_df = load_full_data(url)
+
+    # Clean data
+    clean_data_df = clean_data(raw_data_df)
+
+    # Transform data
+    transform_data_df = clean_data(clean_data_df)
+
+    # Render Graph inside html page:
+
+    return raw_data_df
+
+
+if __name__ == '__main__':
+    api_url = 'https://data.lacity.org/resource/2nrs-mtv8.json?$where=date_rptd>"2023-01-01T00:00:00.000"&$limit=80000'
+
+    # load crimes to data freame
+    df = main(api_url)
+
+    print(df.head(5))
+    print(f'Max date inside dataframe : {df.date_rptd.max()}')
